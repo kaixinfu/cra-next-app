90b5253e0eac28fcd2a15de7b762241a5ea898b7
liukaixin <1360517661@qq.com>
2020-07-12 10:28:06 +0800

    验证 403

---
+++ pages/uc.vue
@@ -0,0 +1,33 @@
+ <template>
+    <div class="login-container">
+        <h1>用户</h1>
+    </div>
+</template>
+<script>
+import md5 from "md5";
+export default {
+    layout: "login",
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    async mounted() {
+        let res = await this.$http.get("/user/info")
+        console.log("res", res);
+        
+    }
+}
+</script> 
+
+<style lang="stylus">
+.login-form
+    width 800px
+    margin 50px auto
+</style>
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,18 +1,38 @@
 import Vue from 'vue'
 import axios from 'axios'
+import {MessageBox} from "element-ui"
 
 const service = axios.create({
     baseURL: "/api"
 })
 
-// 请求拦截 token
-
-// 响应拦截
-service.interceptors.response.use(async res => {
-    let {data} = res
-    return data
-})
-
+export default ({store, redirect}) => {
+    // 请求拦截 token
+    service.interceptors.request.use(async config => {
+        let token = localStorage.getItem("token")
+        if (token) {
+            config.headers.common['Authorization'] = 'Bearer ' + token
+        }
+        return config
+    })
+    // 响应拦截
+    service.interceptors.response.use(async res => {
+        let {data} = res
+        switch(data.code) {
+            case 401:
+                MessageBox.confirm("登录失效", "失效", {
+                    confirmButtonText: "",
+                    showCancelButton: false,
+                    type: "warning"
+                }).then(res => {
+                    localStorage.removeItem("token")
+                    redirect({path: "/login"})
+                })
+                return
+        }
+        return data
+    })
+}
 Vue.prototype.$http = service;
 
 export const http = service
